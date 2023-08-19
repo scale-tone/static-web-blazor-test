8a53a66092bd2e7aa673dd194b2043aefa123522
Konstantin Lepeshenkov <5447190+scale-tone@users.noreply.github.com>
2023-08-20 00:43:30 +0200

    api and button

---
+++ api/HttpTest.cs
@@ -0,0 +1,23 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace StaticWebAppTestApi
+{
+    public static class HttpTest
+    {
+        [FunctionName("HttpTest")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            return new OkObjectResult("OK!");
+        }
+    }
+}
